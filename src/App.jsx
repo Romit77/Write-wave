6e479fc477df14837777ca4a5d29527ff7ffa434
Romit  Dey <romit77dey@gmail.com>
2024-02-03 20:07:32 +0530

    Add Redux  and authentication functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
-import "./App.css";
+import { useDispatch } from "react-redux";
 import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { useState, useEffect } from "react";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  return (
-    <>
-      <h1 className="bg-gray-500 text-center font-bold text-3xl p-5">
-        Write Wave
-      </h1>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+    return () => {};
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>{/* <Outlet /> */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
