46db55ae374a5dd84ca332c034f807eca26f0bdd
Romit  Dey <romit77dey@gmail.com>
2024-02-06 14:29:44 +0530

    Refactor imports in components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import { Container, LogoutBtn, Logo } from "../index";
 import { Link } from "react-router-dom";
-import { UseSelector, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 const Header = () => {
